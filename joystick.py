f34064cde4bf239412e355fa7f53641b7c52462c
wansik5991 <kevin103@daum.net>
2021-09-30 22:06:06 +0900

    Update joystick.py

--- joystick.py
+++ joystick.py
@@ -2,18 +2,44 @@ import os
 os.system('cls')
 
 def solution(name):
-    path = [0]
-    length = 0
-    cnt = list(map(lambda x : abs(ord('A') - ord(x)), name))
-    for i in range(len(name)) :
-        if cnt[i] != 0 :
-            path.append(i)
-            length += min(path[-1] + path[-2], len(name)-1 - path[-1])
-        print(path, length)
-        len_from_current = len(name) - 1 - i
-    answer = 0
+    current = 0
+    cnt = list(map(lambda x : min(ord(x) - ord('A'), ord('Z') - ord(x) + 1), name))
+
+    for i in range(len(cnt)) :
+        if cnt[i] != 0: 
+            current, answer = i, i; 
+            break;
+            
+    while True :
+        answer += cnt[current]
+        cnt[current]=0
+
+        if all(i == 0 for i in cnt) :
+            break;
+
+        left, right = current -1, (current +1) % len(name)
+        dist_left, dist_right = 1,1
+
+        while cnt[left] == 0 : 
+            dist_left += 1; 
+            left -= 1;
+        while cnt[right] == 0 : 
+            dist_right +=1; 
+            right = (right+1) % len(name);
+
+
+        if dist_left > dist_right : 
+            current = right
+            answer += dist_right
+        else : 
+            current = len(name) + left if left < 0 else left
+            answer += dist_left
+
     return answer
 
+print(solution('ABAAAAAAAAABB'))
+
+print(solution('ZAAAZZZZZZZ'))
 print(solution("JAN"))
 print(solution("JAZ"))
 print(solution("JEROEN"))
